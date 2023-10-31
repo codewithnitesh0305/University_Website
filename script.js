c66194a340ff3b2ae7445f169a0c5d3544d97140
codewithnitesh0305 <niteshkumar91316@gmail.com>
2023-10-31 20:48:43 +0530

    University_Website

---
+++ script.js
@@ -0,0 +1,10 @@
+var navLink = document.getElementById("nav-link");
+
+// Responsive Menu Bar
+function showMenu(){
+    navLink.style.right = "0";
+}
+function hideMenu(){
+    navLink.style.right = "-350px"
+}
+
